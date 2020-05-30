d437a967cf44e923530bc3ed247e7b32e4526e29
ksulli10 <ksulli10@gmail.com>
2020-05-29 21:46:15 -0400

    Removed outdated project.

--- Projects/Series Success Rate, Pass vs Rush/run vs pass plot.R
+++
@@ -1,5 +0,0 @@
-ggplot(temp_df,
-       mapping = aes(x = percentage, y = series_success_rate, color = play_type)) +
-  geom_point() +
-  geom_smooth() +
-  scale_x_continuous(breaks = seq(0, 1, by = .25))
